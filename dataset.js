3f25d266931b3e89a70bf201abc66c4f43fd017c
Teerapath Sujitto <teerapart.s@gmail.com>
2016-05-31 23:09:57 +0700

    Dataset manager added

--- dataset.js
+++ dataset.js
@@ -9,53 +9,87 @@ function getHash(filename){
     hash.setEncoding('hex');
     fd.on('end', function() {
       hash.end();
-      resolve(hash.read()); // the desired sha1sum
+      resolve(hash.read()); // the desired checksum
     });
     // read all file and pipe it (write it) to the hash object
     fd.pipe(hash);
   })
 }
+
 function isAlreadyAdded(checksum){
   return redis.sismemberAsync('zvts:bags',checksum)
   .then((count)=>{
+    // return both hash and it existence
     return [checksum,count];
   });
 }
+
 function isExist(filename){
   return new Promise((resolve,reject)=>{
+    // To check whether it is exist and not a dir
     stats = fs.lstatSync(filename);
     if(stats.isDirectory())throw new Error("This is a directory.");
     resolve(filename);
   });
 }
-function getUntrainedBag(){
-  return redis.srandmemberAsync("zvts:bags:untrained");
-}
-function setCurrentBag(){
-  return getUntrainedBag().then(
-    (untrainedBag)=>{
-      if(untrainedBag==null)throw new Error("EMPTY");
 
+function getUntrainedBag(){
+  return redis.srandmemberAsync("zvts:bags:untrained").then(
+    (bagHash)=>{
+      //Randomly select hash of bag that was not trained yet
+      if(bagHash==null)throw new Error("EMPTY");
+      return bagHash;
     }
-  )
+  );
 }
+
+function setCurrentBag(bagHash){
+  return redis.setAsync("zvts:currentBag",bagHash).then(()=>{
+    return bagHash;
+  });
+}
+
 function getCurrentBag(){
   return redis.getAsync("zvts:currentBag").then(
     (resp)=>{
-      // if(resp==null)
+      return (resp==null)
+        ?getUntrainedBag().then(setCurrentBag)
+        :resp
     }
-  )
+  );
 }
+
+function setAsTrained(){
+
+}
+
+function increaseBagProgress(bagHash){
+  return isAlreadyAdded(bagHash).then((checksum_count)=>{
+    if(checksum_count[1] == 0)throw new Error("Bag doesn't exist");
+    return redis.incrAsync(`zvts:bags:${bagHash}:progress`).
+    then(()=>{
+      // return redis.sremAsync("zvts:bags:untrained",bagHash);
+    });
+  })
+}
+
+function getBagProgress(bagHash){
+  return redis.getAsync(`zvts:bags:${bagHash}`).then((resp)=>{
+    if(resp==null)throw new Error("Bag doesn't exist");
+    return parseInt(resp);
+  })
+}
+
 exports.addBag = function(filename){
   return isExist(filename)
   .then(getHash)
   .then(isAlreadyAdded)
   .then((checksum_count)=>{
-    //If checksum is hit
+    //throw if checksum is hit : File already acknowledge
     if(checksum_count[1]>0)throw new Error("File already exist");
     return redis.saddAsync('zvts:bags',checksum_count[0])
     .then(redis.setAsync(`zvts:bags:${checksum_count[0]}:filename`,filename))
-    .then(redis.saddAsync(`zvts:bags:untrained`,{checksum_count[0]}));
+    .then(redis.saddAsync(`zvts:bags:untrained`,checksum_count[0]));
   });
 }
 
--- dataset.test.js
+++ dataset.test.js
@@ -1,5 +1,5 @@
 GLOBAL.redis = require('./redisClient');
 var dataset = require('./dataset');
 //Add Bag
-dataset.addBag('/home/chiro/w/zeabus_vision_training_server/data/baggy.bag').then(console.log,console.log);
-// dataset.getFrame().then(console.log,console.log);
+// dataset.addBag('/home/chiro/w/zeabus_vision_training_server/data/baggy.bag').then(console.log,console.log);
+dataset.getFrame().then(console.log,console.log);
