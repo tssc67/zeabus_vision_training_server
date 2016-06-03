6c9d9ed035016354747d7518e6aedc741cef8361
Teerapath Sujitto <teerapart.s@gmail.com>
2016-06-03 15:16:10 +0700

    Change sessin storage to redis

--- session.js
+++ session.js
@@ -1,10 +1,12 @@
 const session = require('express-session');
 const cfg = require('config');
 const uuid = require('uuid');
+var RedisStore = require('connect-redis')(session);
 var options = cfg.get("session");
 module.exports = session({
   genid: uuid.v4,
   resave: false,
   saveUninitialized: true,
-  secret: options.secret
+  secret: options.secret,
+  store: new RedisStore()
 })
